2458e4606f8755982d5266abb3e964ebfa4dd8f8
spasanatol <spasanatol@DESKTOP-ATILEIU>
2022-05-06 18:40:22 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new ForRealization().ReturnMethod();
+            Console.ReadLine();
+        }
+    }
+    class MyList<T>
+    {
+        public List<T> someList;
+        
+        public MyList() => someList = new List<T>();
+        public void AddMethod(T [] arratList)
+        {
+            Console.WriteLine("Adding elements..");
+            someList.AddRange(arratList);
+            
+        }
+        public void DeleteMethod()
+        {
+            Console.WriteLine("Enter index to remove: ");
+            int argument = Convert.ToInt32(Console.ReadLine());
+            someList.RemoveAt(argument);
+            Console.Write("\n");
+        }
+        public void OutputMethod()
+        {
+            Console.WriteLine("List output: ");
+            foreach (var item in someList)
+            {
+                Console.Write("\n");
+                Console.Write(item);
+            }
+        }
+        public int Count => someList.Count;
+
+        public T this[int index] => someList[index];
+        
+    }
+    class ForRealization
+    {
+        public void ReturnMethod()
+        {
+          
+            int[] arrayList = new int[7] { 9, 4, -7, 2, 5, 10,3 };
+            MyList<int> myList = new MyList<int>();
+            myList.AddMethod(arrayList);
+            myList.DeleteMethod();
+            myList.OutputMethod();
+
+            Console.Write("\n");
+            Console.WriteLine($"All elements in list = {myList.Count}");
+            Console.Write("\n");
+            Console.WriteLine("Enter index: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"This is {myList[index]}");
+
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new ForReal().DictMethod();
+            Console.ReadLine();
+        }
+    }
+    class MyDictionary<TKey, TValue>
+    {
+        Dictionary<TKey, TValue> dict = new Dictionary<TKey, TValue>();
+        public void Add(TKey arg1, TValue arg2, TKey arg3, TValue arg4)
+        {
+            dict.Add(arg1, arg2);
+            dict.Add(arg3, arg4);
+        }
+
+        public int Count { get => dict.Count; }
+
+        public void Method()
+        {
+            foreach (KeyValuePair<TKey, TValue> keyValuePair in dict)
+            {
+                Console.WriteLine(keyValuePair.Key + "-" + keyValuePair.Value);
+            }
+        }
+    }
+    class ForReal
+    {
+        public void DictMethod()
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            try
+            {
+                Console.WriteLine("Enter first key: ");
+                int key1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter first value: ");
+                string value1 = Console.ReadLine();
+                Console.Write("\n");
+                Console.WriteLine("Enter second key: ");
+                int key2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second value: ");
+                string value2 = Console.ReadLine();
+
+                myDictionary.Add(key1, value1, key2, value2);
+                Console.Write("\n");
+                myDictionary.Method();
+                Console.Write("\n");
+                Console.WriteLine($"All elements = {myDictionary.Count}");
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+    }
+}
